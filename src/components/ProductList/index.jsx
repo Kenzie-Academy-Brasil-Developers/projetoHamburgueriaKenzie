6218ef69dbc68be7c0af8cb7145eaa5edd5a5eaf
gabizinha <gabrielamiyazaki55@gmail.com>
2023-08-10 17:08:51 -0300

    atualizacoes de funcoes, adicionado novas funcionalidades

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,15 +1,22 @@
 import { ProductCard } from "./ProductCard";
 import styles from "./style.module.scss"
 export const ProductList = ({ productsSearch,addItem }) => {
-   
+   console.log(productsSearch)
    return (
       <section className={styles.sectionCards}>
          <div className={styles.divSection}>
+            {productsSearch.length === 0 ? (
+               <div className={styles.divMessageSearch}>
+                  <h2 className="typography messageSearch">Desculpe, não encontramos nenhum produto correspondente à sua pesquisa.</h2>
+               </div>
+            ) : (
                <ul className={styles.ulCard}>
                   {productsSearch.map((product) => (
-                     <ProductCard key={product.id} product={product}  addItem={addItem} />
-                  ))}
+                        <ProductCard key={product.id} product={product}  addItem={addItem} />
+                     ))}
                </ul>
+            )}
+               
             </div>
          
       </section>
